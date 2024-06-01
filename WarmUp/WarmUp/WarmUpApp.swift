fd781d5be2e25ef19f50db70d838d77c75d43dac
Yumin Chu <yuminc03@gmail.com>
2024-06-01 23:17:06 +0900

    Chore; 기존 불필요한 폴더 삭제, WarmUp 프로젝트 생성

---
+++ WarmUp/WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by Yumin Chu on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
