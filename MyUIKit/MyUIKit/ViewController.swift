796152042f3aab55cca93268a176a8c11a61d7ed
Yumin Chu <yuminc03@gmail.com>
2024-06-25 17:00:51 +0900

    Feat: 중간저장 - Storyboard 프로젝트 새로 만들고 Storyboard에 component 배치

---
+++ MyUIKit/MyUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUIKit
+//
+//  Created by LS-NOTE-00106 on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
