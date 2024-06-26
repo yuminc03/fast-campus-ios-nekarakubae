04a263267afb4d767c0e90c3dbd5ce374271a4d1
Yumin Chu <yuminc03@gmail.com>
2024-06-26 09:15:49 +0900

    Feat: Main storybaord에 VC 추가, initialVC로 설정, 새로운 VC 클래스 연결하기

---
+++ MyUIKit/MyUIKit/MyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyViewController.swift
+//  MyUIKit
+//
+//  Created by LS-NOTE-00106 on 6/26/24.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+  
+}
