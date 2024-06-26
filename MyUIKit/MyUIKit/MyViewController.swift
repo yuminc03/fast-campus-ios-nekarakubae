1cd18cea53631c42e0bd69f57b3a3cfed3d90b9c
Yumin Chu <yuminc03@gmail.com>
2024-06-26 09:46:31 +0900

    Feat: 버튼 누르면 TextField에 입력한 Text를 Label에 넣기, storyboard autolayout 잡기

--- MyUIKit/MyUIKit/MyViewController.swift
+++ MyUIKit/MyUIKit/MyViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class MyViewController: UIViewController {
   
+  @IBOutlet weak var helloLabel: UILabel!
+  @IBOutlet weak var nameTextField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .red
   }
   
+  @IBAction func didTapButton(_ sender: Any) {
+    if let name = nameTextField.text {
+      helloLabel.text = "Hello \(name)!"
+    }
+  }
 }
