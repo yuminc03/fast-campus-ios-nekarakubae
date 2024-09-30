1d815b8f60a93ceaad3bc84872c5f8d0d72a6ea1
Yumin Chu <yuminc03@gmail.com>
2024-09-30 23:58:35 +0900

    Feat: 프로젝트 첫 생성하기

---
+++ VoiceMemo/VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by Yumin Chu on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
