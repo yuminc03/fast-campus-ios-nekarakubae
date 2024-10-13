1e7894267403b5bdc6b3ad76d8ab9583bedc5732
Yumin Chu <yuminc03@gmail.com>
2024-10-13 22:16:57 +0900

    Feat: TodoListVM 구현하기

--- VoiceMemo/VoiceMemo/Feature/Home/TodoList/TodoListVM.swift
+++ VoiceMemo/VoiceMemo/Feature/Home/TodoList/TodoListVM.swift
@@ -1,5 +1,80 @@
 import Foundation
 
-class TodoListVM {
+final class TodoListVM: ObservableObject {
+  @Published var todos: [Todo]
+  @Published var isEditTodoMode: Bool
+  @Published var removeTodos: [Todo]
+  @Published var isDisplayRemoveTodoAlert: Bool
   
+  var removeTdosCount: Int {
+    return removeTodos.count
+  }
+  
+  var navigationBarRightButtonMode: NavigationButtonType {
+    return isEditTodoMode ? .complete : .edit
+  }
+  
+  init(
+    todos: [Todo],
+    isEditTodoMode: Bool = false,
+    removeTodos: [Todo],
+    isDisplayRemoveTodoAlert: Bool = false
+  ) {
+    self.todos = todos
+    self.isEditTodoMode = isEditTodoMode
+    self.removeTodos = removeTodos
+    self.isDisplayRemoveTodoAlert = isDisplayRemoveTodoAlert
+  }
+}
+
+extension TodoListVM {
+  /// todo의 checkBox를 눌렀을 때
+  func didTapSelectedBox(_ todo: Todo) {
+    if let index = todos.firstIndex(where: { $0 == todo }) {
+      todos[index].selected.toggle()
+    }
+  }
+  
+  /// todo 추가
+  func addTodo(_ todo: Todo) {
+    todos.append(todo)
+  }
+  
+  /// Navigation RightButton을 눌렀을 때
+  func tapNavigationRightButton() {
+    if isEditTodoMode {
+      if removeTodos.isEmpty {
+        isEditTodoMode = false
+      } else {
+        setIsDisplayRemoveTodoAlert(true)
+      }
+    } else {
+      isEditTodoMode = true
+    }
+  }
+  
+  /// todo 삭제를 위한 alert을 띄움
+  func setIsDisplayRemoveTodoAlert(_ isDisplay: Bool) {
+    isDisplayRemoveTodoAlert = isDisplay
+  }
+  
+  /// 삭제된 todo checkbox 눌렀을 때
+  func didTapTodoRemoveSelectedBox(_ todo: Todo) {
+    if let index = removeTodos.firstIndex(of: todo) {
+      removeTodos.remove(at: index)
+    } else {
+      removeTodos.append(todo)
+    }
+  }
+  
+  /// 삭제하기 버튼을 눌렀을 때
+  func didTapRemoveButton() {
+    // removeTodos에 포함되어있는 todo들을 삭제
+    todos.removeAll { todo in
+      removeTodos.contains(todo)
+    }
+    
+    removeTodos.removeAll()
+    isEditTodoMode = false
+  }
 }
--- VoiceMemo/VoiceMemo/Model/Todo/Todo.swift
+++ VoiceMemo/VoiceMemo/Model/Todo/Todo.swift
@@ -5,7 +5,7 @@ struct Todo: Hashable {
   let title: String
   let time: Date
   let day: Date
-  let selected: Bool
+  var selected: Bool
   
   var convertedDayAndTime: String {
     // 오늘 - 오후 03:00에 알림
