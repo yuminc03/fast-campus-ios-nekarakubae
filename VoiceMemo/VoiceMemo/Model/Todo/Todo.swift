a4f6cba0ed5faeef375af8708e896005a39a5ba6
Yumin Chu <yuminc03@gmail.com>
2024-10-13 21:47:30 +0900

    Feat: 할 일 목록을 표현할 구조체 추가

---
+++ VoiceMemo/VoiceMemo/Model/Todo/Todo.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// 할 일 목록
+struct Todo: Hashable {
+  let title: String
+  let time: Date
+  let day: Date
+  let selected: Bool
+  
+  var convertedDayAndTime: String {
+    // 오늘 - 오후 03:00에 알림
+    String("\(day.formattedDay) - \(time.formattedTime)에 알림")
+  }
+}
