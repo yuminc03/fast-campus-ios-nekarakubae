e87bf603baa7a07a38c257e553e6f8bfa5f6369c
Yumin Chu <yuminc03@gmail.com>
2024-12-22 21:59:50 +0900

    Feat: TimerVM 구현

--- VoiceMemo/VoiceMemo/Feature/Home/Timer/TimerVM.swift
+++ VoiceMemo/VoiceMemo/Feature/Home/Timer/TimerVM.swift
@@ -1,5 +1,68 @@
 import Foundation
 
-class TimerVM {
+final class TimerVM: ObservableObject {
+  @Published var isDisplaySetTimeView: Bool // time picker가 나타났는지 여부
+  @Published var time: Time
+  @Published var timer: Timer?
+  @Published var timeRemaining: Int
+  @Published var isPaused: Bool
   
+  init(
+    isDisplaySetTimeView: Bool = true,
+    time: Time = .init(hours: 0, minutes: 0, seconds: 0),
+    timer: Timer? = nil,
+    timeRemaining: Int = 0,
+    isPaused: Bool = false
+  ) {
+    self.isDisplaySetTimeView = isDisplaySetTimeView
+    self.time = time
+    self.timer = timer
+    self.timeRemaining = timeRemaining
+    self.isPaused = isPaused
+  }
+}
+
+extension TimerVM {
+  func didTapSettingButton() {
+    isDisplaySetTimeView = false
+    timeRemaining = time.convertedSeconds
+    startTimer()
+  }
+  
+  func didTapCancelButton() {
+    stopTimer()
+    isDisplaySetTimeView = true
+  }
+  
+  func didTapPauseOrRestartButton() {
+    if isPaused {
+      startTimer()
+    } else {
+      timer?.invalidate()
+      timer = nil
+    }
+    
+    isPaused.toggle()
+  }
+}
+
+private extension TimerVM {
+  func startTimer() {
+    guard timer == nil else { return }
+    
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+      guard let self else { return }
+      
+      if timeRemaining > 0 {
+        timeRemaining -= 1
+      } else {
+        stopTimer()
+      }
+    }
+  }
+  
+  func stopTimer() {
+    timer?.invalidate()
+    timer = nil
+  }
 }
