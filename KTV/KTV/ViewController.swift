e185a8a09f6d19452c9fcd2800de8348790b9fda
Yumin Chu <yuminc03@gmail.com>
2024-12-26 16:48:01 +0900

    Feat: KTV 프로젝트 새로 생성

---
+++ KTV/KTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KTV
+//
+//  Created by LS-NOTE-00106 on 12/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ KTV/KTVUITests/KTVUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  KTVUITestsLaunchTests.swift
+//  KTVUITests
+//
+//  Created by LS-NOTE-00106 on 12/26/24.
+//
+
+import XCTest
+
+final class KTVUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
