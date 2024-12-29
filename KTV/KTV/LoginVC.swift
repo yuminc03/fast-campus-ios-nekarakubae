3c8cce4195d12ea504bb8f98ff30dbb2ea109911
Yumin Chu <yuminc03@gmail.com>
2024-12-29 21:03:41 +0900

    Feat: 로그인 화면 디자인, LaunchScreen 구성

---
+++ KTV/KTV/LoginVC.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+final class LoginVC: UIViewController {
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var loginButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupUI()
+  }
+  
+  private func setupUI() {
+    loginButton.layer.cornerRadius = 20
+    loginButton.layer.borderColor = UIColor(resource: .mainBrown).cgColor
+    loginButton.layer.borderWidth = 1
+  }
+  
+  @IBAction func didTapLoginButton(_ sender: Any) {
+    
+  }
+}
+
