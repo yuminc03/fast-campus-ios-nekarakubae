3cb1b6d3aeaae98708c9f3b4cb888033463c60f6
Yumin Chu <yuminc03@gmail.com>
2025-01-08 21:46:06 +0900

    Feat: HomeVideoCell - setData 메서드

--- KTV/KTV/Home/Cell/HomeVideoCell.swift
+++ KTV/KTV/Home/Cell/HomeVideoCell.swift
@@ -11,8 +11,11 @@ final class HomeVideoCell: UITableViewCell {
   @IBOutlet weak var channelImageView: UIImageView!
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var subTitleLabel: UILabel!
-  @IBOutlet weak var channelTitleLabel: UIView!
+  @IBOutlet weak var channelTitleLabel: UILabel!
   @IBOutlet weak var channelSubTitleLabel: UILabel!
+  
+  private var thumbnailTask: Task<Void, Never>?
+  private var channelThumbnailTask: Task<Void, Never>?
   
   // XIB 파일이 인스턴스로 만들어지고 UI와 이 class가 서로 link되었을 때 호출
   override func awakeFromNib() {
@@ -29,4 +32,25 @@ final class HomeVideoCell: UITableViewCell {
     // Configure the view for the selected state
   }
   
+  func setData(_ data: Home.Video) {
+    titleLabel.text = data.title
+    subTitleLabel.text = data.subtitle
+    channelTitleLabel.text = data.channel
+    channelSubTitleLabel.text = data.channelDescription
+    hotImageView.isHidden = data.isHot == false
+    thumbnailTask = .init(operation: {
+      guard let responseData = try? await URLSession.shared.data(for: .init(url: data.imageURL)).0 else {
+        return
+      }
+      
+      thumbnailImageView.image = UIImage(data: responseData)
+    })
+    channelThumbnailTask = .init(operation: {
+      guard let responseData = try? await URLSession.shared.data(for: .init(url: data.channelThumbnailURL)).0 else {
+        return
+      }
+      
+      channelImageView.image = UIImage(data: responseData)
+    })
+  }
 }
