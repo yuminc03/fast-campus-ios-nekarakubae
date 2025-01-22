1c31ec3a48dce45cac39541fc2c2efc9b6c80acc
Yumin Chu <yuminc03@gmail.com>
2025-01-22 22:31:33 +0900

    Feat: vm에 HomeRecommendVM에 대한 내용 추가

--- KTV/KTV/Home/HomeVM.swift
+++ KTV/KTV/Home/HomeVM.swift
@@ -3,12 +3,16 @@ import Foundation
 @MainActor
 final class HomeVM {
   private(set) var home: Home?
+  
+  let recommendVM: HomeRecommendVM = .init()
   var dataChanged: (() -> Void)?
   
   func requestData() {
     Task {
       do {
-        self.home = try await DataLoader.load(url: URLDefines.home, for: Home.self)
+        let home = try await DataLoader.load(url: URLDefines.home, for: Home.self)
+        self.home = home
+        self.recommendVM.recommends = home.recommends
         self.dataChanged?()
       } catch {
         print("JSON Parsing Failed: \(error.localizedDescription)")
