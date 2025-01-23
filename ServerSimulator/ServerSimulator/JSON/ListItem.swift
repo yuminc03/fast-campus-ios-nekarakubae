3a9d57b96d6ce316839af3eb432b84bdcf0777a0
Yumin Chu <yuminc03@gmail.com>
2025-01-23 22:21:07 +0900

    Chore: ServerSimulator ListItem 필요없는 주석 삭제

--- ServerSimulator/ServerSimulator/JSON/ListItem.swift
+++ ServerSimulator/ServerSimulator/JSON/ListItem.swift
@@ -1,22 +1,17 @@
-//
-//  ListItem.swift
-//  ServerSimulator
-//
-
 import Foundation
 
 struct ListItem: Codable {
-    let videoId: Int
-    let imageUrl: URL
-    let title: String
-    let playtime: Double
-    let channel: String
-    
-    init(videoId: Int, title: String, playtime: Double, channel: String) {
-        self.videoId = videoId
-        self.imageUrl = URLDefinition.listThumbnailUrl
-        self.title = title
-        self.playtime = playtime
-        self.channel = channel
-    }
+  let videoId: Int
+  let imageUrl: URL
+  let title: String
+  let playtime: Double
+  let channel: String
+  
+  init(videoId: Int, title: String, playtime: Double, channel: String) {
+    self.videoId = videoId
+    self.imageUrl = URLDefinition.listThumbnailUrl
+    self.title = title
+    self.playtime = playtime
+    self.channel = channel
+  }
 }
