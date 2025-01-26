6570d1130e623a030a76fe8f7650c952c326c1c4
Yumin Chu <yuminc03@gmail.com>
2025-01-26 23:51:57 +0900

    Feat: HomeRankingHeaderView 추가

---
+++ KTV/KTV/Home/Cell/HomeRankingHeaderView.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class HomeRankingHeaderView: UICollectionReusableView {
+  static let height: CGFloat = 63
+  static let id = "HomeRankingHeaderView"
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+}
