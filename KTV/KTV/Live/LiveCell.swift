64b2e50452d8bcea261a4eb804699cb8477bb1a3
Yumin Chu <yuminc03@gmail.com>
2025-02-13 22:52:57 +0900

    Feat: LiveCell XIB 파일 UI 배치, IBOutlet 연결

--- KTV/KTV/Live/LiveCell.swift
+++ KTV/KTV/Live/LiveCell.swift
@@ -1,9 +1,22 @@
 import UIKit
 
 final class LiveCell: UICollectionViewCell {
+  static let height: CGFloat = 76
+  static let id = "LiveCell"
+  
+  @IBOutlet var titleLabel: UILabel!
+  @IBOutlet var descriptionLabel: UILabel!
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var liveLabel: UILabel!
+  private var imageTask: Task<Void, Never>?
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    // Initialization code
+    
+    setupUI()
+  }
+  
+  private func setupUI() {
+    
   }
 }
