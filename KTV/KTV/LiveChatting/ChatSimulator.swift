44c5c800c35ab1600197891a9e927c40b1089be5
Yumin Chu <yuminc03@gmail.com>
2025-07-10 22:43:53 +0900

    Feat: CoreSimulator - 메시지 멘트를 랜덤으로 반환하는 computed property 추가

---
+++ KTV/KTV/LiveChatting/ChatSimulator.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+struct ChatMessage {
+  let isMine: Bool
+  let text: String
+}
+
+@MainActor
+final class ChatSimulator {
+  typealias MessageHandler = ((ChatMessage) -> Void)
+  
+  private var callback: MessageHandler?
+  private var task: Task<Void, Never>?
+  
+  private var randomMessage: String {
+    let messages = [
+      "안녕하세요!",
+      "모두 안녕하세요!",
+      "안녕하신가요?",
+      "안녕하십니까?",
+      "저는 새로운 회원입니다.",
+      "반갑습니다. 제 이름은 [이름]이에요.",
+      "안녕하세요! 함께 활동하려고 합니다.",
+      "감사합니다!",
+      "고맙습니다!",
+      "정말로 감사합니다!",
+      "도움이 되어서 감사합니다.",
+      "어떤 의견이 있나요?",
+      "여기 무슨 이야기가 진행되고 있나요?",
+      "무엇을 이야기하고 싶으세요?",
+      "그렇군요.",
+      "정말로?",
+      "흥미로운 주제네요!",
+      "이야기하고 싶어요.",
+      "잘 가요!",
+      "다음에 또 만나요!",
+      "즐거운 시간 되세요!",
+      "안녕히 가세요!",
+      "도와주세요!",
+      "어떻게 해야 할까요?",
+      "도움이 필요해요!",
+      "도움이 필요한데, 누가 도와줄 수 있을까요?",
+    ]
+    
+    return messages[Int.random(in: 0 ..< messages.count)]
+  }
+  
+  deinit {
+    self.task?.cancel()
+  }
+  
+  func start() {
+    
+  }
+  
+  func stop() {
+    
+  }
+  
+  func setMessageHandler(_ handler: MessageHandler?) {
+    
+  }
+}
