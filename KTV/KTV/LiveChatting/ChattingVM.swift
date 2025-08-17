41614c10edbff5337742f3e42036062e849bf53c
Yumin Chu <yuminc03@gmail.com>
2025-08-17 22:08:40 +0900

    Feat: ChattingVM에서 ChatSimulator 사용하여 채팅 메시지 dummy 가져오기

--- KTV/KTV/LiveChatting/ChattingVM.swift
+++ KTV/KTV/LiveChatting/ChattingVM.swift
@@ -2,5 +2,26 @@ import Foundation
 
 @MainActor
 final class ChattingVM {
+  private let chatSimulator = ChatSimulator()
+  private(set) var messages: [ChatMessage] = []
+  var chatReceived: (() -> Void)?
    
+  init() {
+    self.chatSimulator.setMessageHandler { [weak self] in
+      self?.messages.append($0)
+      self?.chatReceived?()
+    }
+  }
+  
+  func start() {
+    chatSimulator.start()
+  }
+  
+  func stop() {
+    chatSimulator.stop()
+  }
+  
+  func sendMessage(_ message: String) {
+    chatSimulator.sendMessage(message)
+  }
 }
