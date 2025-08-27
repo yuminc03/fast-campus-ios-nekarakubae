fc297a85bf5f47e75a3de21cf59525caa9a2637f
Yumin Chu <yuminc03@gmail.com>
2025-08-27 15:25:03 +0900

    Feat: TabBarController - videoVCDelegate를 채택 후 프로토콜의 메서드 호출

--- KTV/KTV/TabBarController.swift
+++ KTV/KTV/TabBarController.swift
@@ -1,13 +1,44 @@
 import UIKit
 
-final class TabBarController: UITabBarController {
+final class TabBarController: UITabBarController, VideoVCDelegate {
+  weak var videoVC: VideoVC?
+  
+  // 휴대폰 방향 세로로 고정
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    return .portrait
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
-  // 휴대폰 방향 세로로 고정
-  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-    return .portrait
+  func videoVC(
+    _ vc: VideoVC,
+    yPositionForMinizeView height: CGFloat
+  ) -> CGFloat {
+    // tabBar위에 배치
+    return tabBar.frame.minY - height
+  }
+  
+  func videoVCDidMinimize(_ vc: VideoVC) {
+    videoVC = vc
+    addChild(vc)
+    view.addSubview(vc.view)
+    vc.didMove(toParent: self)
+  }
+  
+  func videoVCNeedsMaximize(_ vc: VideoVC) {
+    videoVC = nil
+    vc.willMove(toParent: nil)
+    vc.view.removeFromSuperview()
+    vc.removeFromParent()
+    present(vc, animated: true)
+  }
+  
+  func videoVCDidTapClose(_ vc: VideoVC) {
+    vc.willMove(toParent: nil)
+    vc.view.removeFromSuperview()
+    vc.removeFromParent()
+    videoVC = nil
   }
 }
