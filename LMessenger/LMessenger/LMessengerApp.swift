1199879ce3e3944c677791188d1d02e827ba87dc
Yumin Chu <yuminc03@gmail.com>
2026-03-04 11:55:48 +0900

    feat: 앱 시작점에 container 사용

--- LMessenger/LMessenger/LMessengerApp.swift
+++ LMessenger/LMessenger/LMessengerApp.swift
@@ -1,17 +1,13 @@
-//
-//  LMessengerApp.swift
-//  LMessenger
-//
-//  Created by Yumin Chu on 3/4/26.
-//
-
 import SwiftUI
 
 @main
 struct LMessengerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @StateObject private var container: DIContainer = .init(services: Services())
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environmentObject(container)
     }
+  }
 }
