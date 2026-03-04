0d24e31b368f93fd8a9823c6d2fefc29f0f75a3f
Yumin Chu <yuminc03@gmail.com>
2026-03-04 11:28:24 +0900

    setup: L사 메신저앱 Project 생성

---
+++ LMessenger/LMessenger/LMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMessengerApp.swift
+//  LMessenger
+//
+//  Created by Yumin Chu on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
