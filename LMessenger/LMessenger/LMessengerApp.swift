b14e52b592d6c2a4a1e41267072975337f8c926c
Yumin Chu <yuminc03@gmail.com>
2026-03-05 14:51:18 +0900

    feat: Firebase 사용을 위한 기본설정

--- LMessenger/LMessenger/LMessengerApp.swift
+++ LMessenger/LMessenger/LMessengerApp.swift
@@ -2,11 +2,12 @@ import SwiftUI
 
 @main
 struct LMessengerApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
   @StateObject private var container: DIContainer = .init(services: Services())
   
   var body: some Scene {
     WindowGroup {
-      AuthenticatedView(vm: .init())
+      AuthenticatedView(vm: .init(container: container))
         .environmentObject(container)
     }
   }
