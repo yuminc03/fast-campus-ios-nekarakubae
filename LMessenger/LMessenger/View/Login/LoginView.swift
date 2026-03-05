3f305f18b85f7e3de3dd141217cee2348a657083
Yumin Chu <yuminc03@gmail.com>
2026-03-05 15:58:07 +0900

    feat: 로그인 화면에 Google 로그인, Apple 로그인 버튼 추가, back 버튼 추가

--- LMessenger/LMessenger/View/Login/LoginView.swift
+++ LMessenger/LMessenger/View/Login/LoginView.swift
@@ -1,18 +1,53 @@
 import SwiftUI
 
 struct LoginView: View {
+  @Environment(\.dismiss) var dismiss
+  
   var body: some View {
     VStack(alignment: .leading) {
-      Text("로그인")
-        .font(.system(size: 28, weight: .bold))
-        .foregroundStyle(.bkText)
-        .padding(.top, 80)
-      
-      Text("아래 제공되는 서비스로 로그인을 해주세요.")
-        .font(.system(size: 14))
-        .foregroundStyle(.greyDeep)
+      Group {
+        Text("로그인")
+          .font(.system(size: 28, weight: .bold))
+          .foregroundStyle(.bkText)
+          .padding(.top, 80)
+        
+        Text("아래 제공되는 서비스로 로그인을 해주세요.")
+          .font(.system(size: 14))
+          .foregroundStyle(.greyDeep)
+      }
+      .padding(.horizontal, 30)
       
       Spacer()
+      
+      Button {
+        
+      } label: {
+        Text("Google로 로그인")
+      }
+      .buttonStyle(LoginButtonStyle(
+        textColor: .bkText,
+        borderColor: .greyLight
+      ))
+      
+      Button {
+        
+      } label: {
+        Text("Apple로 로그인")
+      }
+      .buttonStyle(LoginButtonStyle(
+        textColor: .bkText,
+        borderColor: .greyLight
+      ))
+    }
+    .navigationBarBackButtonHidden()
+    .toolbar {
+      ToolbarItemGroup(placement: .navigationBarLeading) {
+        Button {
+          dismiss()
+        } label: {
+          Image(.back)
+        }
+      }
     }
   }
 }
